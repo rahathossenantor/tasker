7bba481bf2ea291b916fbf34f138164822311a3d
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2025-10-03 18:25:20 +0600

    Footer section is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from "./sections/Footer";
 import Header from "./sections/Header";
 
 export default function App() {
     return (
         <>
             <Header />
+            <Footer />
         </>
     );
 }
